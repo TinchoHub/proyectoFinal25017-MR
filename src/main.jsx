8209ad47945979907fc3c14740ba0b874f8f0f79
Martin Roige <martinroige@hotmail.com>
2025-07-20 13:59:16 -0300

    Otras modificaciones menores

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { HelmetProvider } from 'react-helmet-async';
-import './styles/index.css'
-// import 'bootstrap/dist/css/bootstrap.min.css'
+import './styles/index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
